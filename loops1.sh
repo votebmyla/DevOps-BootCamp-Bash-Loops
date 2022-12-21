5fa47a656d0209e7e840fa9b910288b62936138e
votebmyla <votebmyla@gmail.com>
2022-12-21 14:58:46 +0500

    add task solution

--- loops1.sh
+++ loops1.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 # Put your code here
+
+# input variable 
+# uppercase to lowercase, lowercase to uppercase 
+str=${1~~}
+
+# string length
+str_len=${#str}
+
+# last index number of string
+last_char=$(($str_len - 1))
+
+# variable for reverse string
+rts=""
+
+# iterate string
+for i in ""$(seq 0 $last_char)""; do
+  # concatenate $i variable value to $rts variable
+  rts="${str:$i:1}$rts"
+done
+
+echo $rts
